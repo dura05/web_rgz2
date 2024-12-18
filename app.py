704cc93b5b966b3652a11617cfeaadb140d3c4f3
Вадик <vadik@MBP--Ira.IGD_Rostelecom>
2024-12-18 19:35:30 +0700

    хостинг

--- app.py
+++ app.py
@@ -13,15 +13,22 @@ app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
 
 def db_connect():
-    conn = psycopg2.connect(
-        host='127.0.0.1',
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
         database='cinema',
         user='vadik',
         password='vadik2004'
-    )
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    return conn, cur
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
 
+    return conn, cur
 
 def db_close(conn, cur):
     conn.commit()
